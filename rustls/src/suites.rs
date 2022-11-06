2e0f14e27ed1c05dc7857828170e52846874c367
Robin <70903912+Robeartt@users.noreply.github.com>
2022-11-06 16:05:49 +0100

    Shuffle order of extensions

--- rustls/src/suites.rs
+++ rustls/src/suites.rs
@@ -11,9 +11,22 @@ use crate::tls12::{
     // TLS1.2 suites
     TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
     TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+
+    // unimplemented
+    TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+
     TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+
+    // unimplemented
+    TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+
     TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
     TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+
+    TLS_RSA_WITH_AES_128_CBC_SHA,
+    TLS_RSA_WITH_AES_128_GCM_SHA256,
+    TLS_RSA_WITH_AES_256_CBC_SHA,
+    TLS_RSA_WITH_AES_256_GCM_SHA384,
 };
 use crate::tls13::Tls13CipherSuite;
 use crate::tls13::{
@@ -149,12 +162,35 @@ pub static ALL_CIPHER_SUITES: &[SupportedCipherSuite] = &[
 /// shouldn't be enabled by most applications.
 pub static DEFAULT_CIPHER_SUITES: &[SupportedCipherSuite] = ALL_CIPHER_SUITES;
 
+static UNIMPLEMENTED_CIPHER_SUTIES: &[SupportedCipherSuite] = &[
+    #[cfg(feature = "tls12")]
+        TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+    #[cfg(feature = "tls12")]
+        TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+    #[cfg(feature = "tls12")]
+        TLS_RSA_WITH_AES_128_GCM_SHA256,
+    #[cfg(feature = "tls12")]
+        TLS_RSA_WITH_AES_256_GCM_SHA384,
+    #[cfg(feature = "tls12")]
+        TLS_RSA_WITH_AES_128_CBC_SHA,
+    #[cfg(feature = "tls12")]
+        TLS_RSA_WITH_AES_256_CBC_SHA,
+];
+
 // These both O(N^2)!
 pub(crate) fn choose_ciphersuite_preferring_client(
     client_suites: &[CipherSuite],
     server_suites: &[SupportedCipherSuite],
 ) -> Option<SupportedCipherSuite> {
-    for client_suite in client_suites {
+    let unimplemented_cipher_suite: Vec<_> = UNIMPLEMENTED_CIPHER_SUTIES
+        .iter()
+        .map(|c| c.suite())
+        .collect();
+
+    for client_suite in client_suites
+        .iter()
+        .filter(|c| !unimplemented_cipher_suite.contains(c))
+    {
         if let Some(selected) = server_suites
             .iter()
             .find(|x| *client_suite == x.suite())
